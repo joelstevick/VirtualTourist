ae80a6c44dbf825d0dc39912bc2b3b894ec94844
joel stevick <joelstevick@outlook.com>
2022-05-17 04:51:41 -0400

    added navigation controllers

---
+++ VirtualTourist/Controllers/ListNavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationViewController.swift
+//  VirtualTourist
+//
+//  Created by Joel Stevick on 5/17/22.
+//
+
+import UIKit
+
+class ListNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "Virtual Tourist"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
