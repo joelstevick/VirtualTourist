4e99371b1106f0ff5d2f48ae8270ed349ae30a32
joel stevick <joelstevick@outlook.com>
2022-05-17 05:09:45 -0400

    longpress to add pin

---
+++ VirtualTourist/Controllers/MapViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Joel Stevick on 5/17/22.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, UIGestureRecognizerDelegate {
+    // MARK: - Properties
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: - Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup longpress for MapView
+        let lpgr = UILongPressGestureRecognizer(target: self,
+                                                action:#selector(self.handleLongPress))
+        lpgr.minimumPressDuration = 1
+        lpgr.delaysTouchesBegan = true
+        lpgr.delegate = self
+        self.mapView.addGestureRecognizer(lpgr)
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    // MARK: - Handlers
+    @objc func handleLongPress(gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state != UIGestureRecognizer.State.ended {
+            return
+        }
+        else if gestureRecognizer.state != UIGestureRecognizer.State.began {
+            
+            let touchPoint = gestureRecognizer.location(in: self.mapView)
+            
+            let touchMapCoordinate =  self.mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchMapCoordinate
+            annotation.title = "You long pressed here"
+            
+            self.mapView.addAnnotation(annotation)
+        }
+    }
+}
