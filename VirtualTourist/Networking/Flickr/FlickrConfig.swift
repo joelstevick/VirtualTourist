c2a1d04d98666b687349045a52ad1d86aba43042
joel stevick <joelstevick@outlook.com>
2022-05-18 07:26:03 -0400

    checkpoint: working on api

--- VirtualTourist/Networking/Flickr/FlickrConfig.swift
+++ VirtualTourist/Networking/Flickr/FlickrConfig.swift
@@ -11,10 +11,10 @@ import CoreLocation
 struct FlickrConfig {
     private static let apiKey = "4d149edffc818464bc15fe986ba85446"
     private static let secret = "39c51aeceb0be12a"
-    private static let endpoint = "https://api.flickr.com/services"
+    private static let endpoint = "https://api.flickr.com/services/rest"
     private static let service = "flickr.photos.search"
     
     static func makeSearchUrl(coordinate: CLLocationCoordinate2D) -> String {
-        return "\(endpoint)/\(service)?\(apiKey)&lat=\(coordinate.latitude)&lon=\(coordinate.longitude)"
+        return "\(endpoint)/\(service)?api_key=\(apiKey)&lat=\(coordinate.latitude)&lon=\(coordinate.longitude)&nojsoncallback=1"
     }
 }
