bc682054ca5895426183c89a7fa5f95026effb50
joel stevick <joelstevick@outlook.com>
2022-05-19 04:40:33 -0400

    add view controller for image picker

---
+++ VirtualTourist/Controllers/ImagePickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImagePickerViewController.swift
+//  VirtualTourist
+//
+//  Created by Joel Stevick on 5/19/22.
+//
+
+import UIKit
+
+class ImagePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
