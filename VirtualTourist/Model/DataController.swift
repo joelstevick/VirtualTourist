9c6c4488b578d63a7014cdd8c04dc9f6f6b60794
joel stevick <joelstevick@outlook.com>
2022-05-21 07:58:09 -0400

    add DataController and initial CoreData model

---
+++ VirtualTourist/Model/DataController.swift
@@ -0,0 +1,30 @@
+//
+//  DataController.swift
+//  VirtualTourist
+//
+//  Created by Joel Stevick on 5/21/22.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            
+            completion?()
+        }
+    }
+}
