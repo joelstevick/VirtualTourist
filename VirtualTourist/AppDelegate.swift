5b791aa060e91b2c6f9f1669ff11a90b13f5cf30
joel stevick <joelstevick@outlook.com>
2022-05-21 08:08:27 -0400

    initialize DataController in AppDelegate part 2

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let dataController = DataController(modelName: "VirtualTourist")
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        dataController.load {
+            // update to Main
+        }
+        
         return true
     }
 
