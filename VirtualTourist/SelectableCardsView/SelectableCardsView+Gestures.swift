5b4296b96d08834483b3a04299804df953f0fe83
joel stevick <joelstevick@outlook.com>
2022-05-24 04:37:42 -0400

    improve gesture handler for swipe

--- VirtualTourist/SelectableCardsView/SelectableCardsView+Gestures.swift
+++ VirtualTourist/SelectableCardsView/SelectableCardsView+Gestures.swift
@@ -40,23 +40,23 @@ extension SelectableCardsView: UIGestureRecognizerDelegate {
     // swipe
     @objc func swipeGestureFired(_ gestureRecognizer: UISwipeGestureRecognizer) {
         
-        print("Swiped")
-        return
-        // toggle the current selection state
-        if selectionState[pageControl.currentPage] != nil {
-            selectionState[pageControl.currentPage] = !selectionState[pageControl.currentPage]!
-        } else {
-            selectionState[pageControl.currentPage] = true
+        guard let delegate = delegate else {
+            return
         }
         
-        // apply visual cue
-        applyVisualCue(cardIndex: pageControl.currentPage,
-                       visualCue: selectionState[pageControl.currentPage]! ? CardVisualCue.Selected : CardVisualCue.Default)
-        
-        // notify the delegate
-        if let delegate = delegate {
+        guard delegate.canRemoveCards() else {
+            return
+        }
+        if let gestureView = gestureRecognizer.view {
+            
+            // apply visual cue
+            applyVisualCue(cardIndex: pageControl.currentPage,
+                           visualCue: selectionState[pageControl.currentPage]! ? CardVisualCue.Selected : CardVisualCue.Default)
+            
+            // notify the delegate
             delegate.cardSelectionChanged(card: cards[pageControl.currentPage]!,
                                           selected: selectionState[pageControl.currentPage]!)
+            
         }
     }
 }
