fd5a078a06de3fb15cb66356f68a8a4602f96ef7
joel stevick <joelstevick@outlook.com>
2022-05-24 18:53:53 -0400

    CRUD

---
+++ VirtualTourist/Entities/Location/Location+CRUD.swift
@@ -0,0 +1,70 @@
+//
+//  Location+CRUD.swift
+//  VirtualTourist
+//
+//  Created by Joel Stevick on 5/24/22.
+//
+
+import Foundation
+import CoreData
+import UIKit
+import NanoID
+
+extension Location {
+    func getAll(context: NSManagedObjectContext, viewController: UIViewController) -> [Location]? {
+        do {
+            let locations = try context.fetch(Location.fetchRequest())
+            
+            return locations
+        } catch {
+            showError(viewController: viewController, message: error.localizedDescription)
+            return nil
+        }
+    }
+    
+    func create(
+        latitude: Double,
+        longitude: Double,
+        title: String?,
+        subtitle: String?,
+        context: NSManagedObjectContext,
+        viewController: UIViewController
+    ) -> Location {
+        let location = Location(context: context)
+        location.id = NanoID.generate()
+        location.title = title
+        location.subtitle = subtitle
+        location.longitude = longitude
+        location.latitude = latitude
+        location.creationDate = Date()
+        
+        do {
+            try context.save()
+            
+            return location
+        } catch {
+            showError(viewController: viewController, message: error.localizedDescription)
+            fatalError(error.localizedDescription)
+        }
+        
+    }
+    
+    func update(location: Location, context: NSManagedObjectContext, viewController: UIViewController) {
+        do {
+            try context.save()
+        } catch {
+            showError(viewController: viewController, message: error.localizedDescription)
+            fatalError(error.localizedDescription)
+        }
+    }
+    func delete(location: Location, context: NSManagedObjectContext, viewController: UIViewController) {
+        context.delete(location)
+        
+        do {
+            try context.save()
+        } catch {
+            showError(viewController: viewController, message: error.localizedDescription)
+            fatalError(error.localizedDescription)
+        }
+    }
+}
